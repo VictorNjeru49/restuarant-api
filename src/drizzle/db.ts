f000993cea5852dde2ce43c21eb2c52e5db525f7
victornjeru49 <victornjeru49@gmail.com>
2024-06-13 18:01:20 +0300

    third commit

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,8 +3,9 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from "./schema"
 
-export const client = neon("postgresql://restuarantdb_owner:LxfwP7haEW0K@ep-weathered-frost-a5oh2con.us-east-2.aws.neon.tech/restuarantdb?sslmode=require") //as string
 
+process.env.Database_URL=("postgresql://restuarantdb_owner:LxfwP7haEW0K@ep-weathered-frost-a5oh2con.us-east-2.aws.neon.tech/restuarantdb?sslmode=require")
+export const client = neon(process.env.Database_URL!) //as string
 
 const db = drizzle(client, { schema, logger: true });
 
